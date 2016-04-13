9388b719be431b35c915c03ab3ccc34226e756c3
Marcelo van Kampen <marcelo@wakoopa.com>
2016-04-13 19:43:11 +0200

    Rewrotten using ECMAScript 6, added back /etc/hosts support

--- nodedns.js
+++ nodedns.js
@@ -1,49 +1,44 @@
-var dns = require('native-dns');
-var server = dns.createServer();
-var fs = require('fs');
-var cp = require('child_process');
-var net = require('net');
-var Docker = require('dockerode');
-var child_process = require('child_process');
+"use strict";
+let dns = require('native-dns');
+let server = dns.createServer();
+let fs = require('fs');
+let Docker = require('dockerode');
 
-var docker = new Docker({
+let docker = new Docker({
   socketPath: '/var/run/docker.sock'
 });
 
-var dnsAmap = [];
-var manualMap = [];
-
-var g_ContainerHosts = {};
-var g_OriginalAnswers = {};
+let host_ip = new Map();
 
 function readDockerContainers(){
 
-	docker.listContainers(function (err, containers){
-		if (err) throw err;
-		 
-		containers.forEach(function (containerInfo)
-		{
-			addContainerNames(containerInfo.Id);	  
-		});
-	});
+  docker.listContainers(function (err, containers){
+    if (err) throw err;
+
+    containers.forEach(function (containerInfo) {
+      addContainerNames(containerInfo.Id);
+    });
+
+    updateEtcHosts();
+  });
 
 }
 
 server.on('request', function (request, response) {
-  
-  if (dnsAmap.hasOwnProperty(request.question[0].name))
-  {
-	console.log("[" + request.address.address + "] requests for " + request.question[0].name + "(" +  dnsAmap[request.question[0].name].address + ")");
-	response.answer.push(dnsAmap[request.question[0].name]);	  
-  }
-  else
-  {
-	  console.log("[" + request.address.address + "]" + " request for " + request.question[0].name + " not found");
-  }
 
+  if (host_ip.has(request.question[0].name)) {
+    console.log(`${request.question[0].name} -> ${host_ip.get(request.question[0].name)}`);
+    response.answer.push(dns.A({
+        name: request.question[0].name,
+        address: host_ip.get[request.question[0].name],
+        ttl: 1 //containers can be very short lived
+    }));
+  } else {
+    console.log("[" + request.address.address + "]" + " request for " + request.question[0].name + " not found");
+  }
 
   response.send();
-  
+
 });
 
 server.on('error', function (err, buff, req, res) {
@@ -51,107 +46,95 @@ server.on('error', function (err, buff, req, res) {
 });
 
 process.on('SIGINT', function() {
-    console.log("End");
+  console.log("End");
 
-    process.exit();
+  process.exit();
 });
 
+function removeContainerNames(containerId) {
+  docker.getContainer(containerId).inspect(function (err, container) {
+    if (container == null) return;
+    if (err) throw err;
 
-function removeContainerNames(containerId)
-{
-        for (var j=0;j<g_ContainerHosts[containerId].hns.length;j++)
-	{
-		console.log ("removing " + g_ContainerHosts[containerId].hns[j]);
-		dnsAmap[g_ContainerHosts[containerId].hns[j]] = g_OriginalAnswers[g_ContainerHosts[containerId].hns[j]];
-	}
+    var hn = container.Config.Hostname + "." + container.Config.Domainname;
+    if (container.Config.Domainname === "")
+      hn = container.Config.Hostname;
+
+    console.log("removing " + hn + " " + container.NetworkSettings.IPAddress);
+
+    host_ip.delete(hn);
+    updateEtcHosts();
+  });
 }
 
-function addContainerNames(containerId)
-{
-	docker.getContainer(containerId).inspect(function (err, container) {
+function addContainerNames(containerId) {
+  docker.getContainer(containerId).inspect(function (err, container) {
 
-		if (container == null) return;
+    if (container == null) return;
 
-		if (err) throw err;
-			  
-		var hn = container.Config.Hostname + "." + container.Config.Domainname;
-		if (container.Config.Domainname === "")
-			hn = container.Config.Hostname;
-				  	
-		console.log("adding " + hn + " " + container.NetworkSettings.IPAddress);
-		
-		g_OriginalAnswers[hn] = dnsAmap[hn];
-		dnsAmap[hn] = 
-	    		 dns.A({
-			    name: hn,
-		    	    address: container.NetworkSettings.IPAddress,
-			    ttl: 1 //containers can be very short lived
-			 });
+    if (err) throw err;
 
-		g_ContainerHosts[containerId] = {};
-		g_ContainerHosts[containerId].hns = [];
-		g_ContainerHosts[containerId].hns.push(hn);
+    var hn = container.Config.Hostname + "." + container.Config.Domainname;
+    if (container.Config.Domainname === "")
+      hn = container.Config.Hostname;
 
-		//check if there are other names for this container
-		var cEnv = container.Config.Env;
-		var otherNames = [];
-		for (var k=0;k<cEnv.length;k++)
-		{
-			if (cEnv[k].indexOf('OTHERNAMES')==0)
-			{
-				otherNames = cEnv[k].substr(cEnv[k].indexOf('=')+1).split(',');
-			}
-		}
+    console.log("adding " + hn + " " + container.NetworkSettings.IPAddress);
 
-		for (var j=0;j<otherNames.length;j++)
-		{
-			g_OriginalAnswers[otherNames[j]] = dnsAmap[otherNames[j]];
-			console.log("adding " + otherNames[j] + " " + container.NetworkSettings.IPAddress);
-			dnsAmap[otherNames[j]] =
-                        			dns.A({
-                                                	name: otherNames[j],
-                                                	address: container.NetworkSettings.IPAddress,
-                                                	ttl: 1
-                                                });
+    host_ip.set(hn, container.NetworkSettings.IPAddress);
+    updateEtcHosts();
+  });
+}
 
-			g_ContainerHosts[containerId].hns.push(otherNames[j]);
-		}
-	});
-		
+function updateEtcHosts() {
+  let containerhosts = "";
+
+  host_ip.forEach (function (k,v) {
+    containerhosts += `${k}    ${v}\n`
+  });
+
+  let hosts = fs.readFileSync("/etc/hosts").toString();
+  let hostsbefore = hosts;
+
+  hosts = hosts.replace(/#BEGIN nodednsmanaged[\s\S]*#END nodednsmanaged\r\n/, "");
+
+  if (host_ip.size !== 0) {
+    let containerPart =  "#BEGIN nodednsmanaged\r\n" + containerhosts + "#END nodednsmanaged\r\n";
+    hosts = containerPart + hosts;
+  }
+
+  if (hosts !== hostsbefore) {
+    fs.writeFileSync("/etc/hosts", hosts);
+  }
 }
 
 readDockerContainers();
 
-docker.getEvents(function (err, stream)
-{
-    stream.on('data', function(data)
-    {	
-	var container = JSON.parse(data.toString());
-	
-	switch (container.status)
-	{
-		//case "create": 
-		//case "exec_create":
-		case "start":
-		case "unpause":
-			addContainerNames(container.id);
-		break;
-		//case "kill":
-		//case "stop":
-		//case "destroy":
-		case "pause":
-		case "die":
-			removeContainerNames(container.id);
-		break;
-	}
-	
+docker.getEvents(function (err, stream) {
+  stream.on('data', function(data) {
+    var container = JSON.parse(data.toString());
+
+    switch (container.status) {
+      //case "create":
+      //case "exec_create":
+      case "start":
+      case "unpause":
+        addContainerNames(container.id);
+      break;
+      //case "kill":
+      //case "stop":
+      //case "destroy":
+      case "pause":
+      case "die":
+        removeContainerNames(container.id);
+      break;
+    }
+
+
   });
 });
 
-if (process.argv.length < 2)
-{
-	console.log("Usage: node nodedns.js <IP ADDRESS OF DOCKER0 IFACE>");
+if (process.argv.length < 2) {
+  console.log("Usage: node nodedns.js <IP ADDRESS OF DOCKER0 IFACE>");
 }
-
 
 server.serve(53, process.argv[1]);
